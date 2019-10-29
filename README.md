8a5dde797147a0afdddc1c43b072196b99b5e3fa
Kumar Raja Gattamaneni <38221186+gattamaneni@users.noreply.github.com>
2019-10-29 20:47:35 +0530

    updated to reflect reservation mechanism
    Update the react component to maintain state and manage the status based on user action to Avail or release the seat

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Used for book keeping of the examples tried to understand React library
 ## Exercise 1
 Objective of this exercise is to explore the concept of slow adoption of React components in collaboration with existing UI technologies. 
 In particular this is explored based on the article [Use as little React as required](https://reactjs.org/docs/add-react-to-a-website.html) from React getting started documentation
+
+### Enahncement on default example
+A component to simulate the reservation of a seat is explored.
+Component with following state is developed:
+1. Attribute 'id' to uniquely identify the seat
+2. Attribute 'status' to reflect the status of the seat {Available, Booked}
+>**status** attribute shall be used to derive the styling color for the component
--- like_button.js
+++ like_button.js
@@ -5,18 +5,33 @@ const e = React.createElement;
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = { 
+      id:1,
+      status: 'Idle'
+    };
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
+    let text_area = e(
+      'h1',
+      {key:1},
+      this.state.status
+    )
+    let reserve_button = e(
       'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+      { key:2, onClick: () => this.setState({status: 'Booked'})},
+      'Avail'
+    )
+    let release_button = e(
+      'button',
+      { key:3, onClick: () => this.setState({status: 'Available'})},
+      'Release'
+    )
+    
+    return e(
+      'div',
+      {},
+      [text_area, reserve_button, release_button]
     );
   }
 }
